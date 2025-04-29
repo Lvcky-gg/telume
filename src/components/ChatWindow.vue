84453dbd7ecb35ba27e7705c5e21a03f931c7fd4
lvcky-gg <matadorcrab@gmail.com>
2025-04-29 16:12:17 -0400

    chore: mad science is happening

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Chat</h1>
+    <button @click="getModels" style="margin-bottom: 20%;">Send</button>
       <el-input
     v-model="message"
     style="width: 240px"
@@ -7,9 +7,32 @@
     type="textarea"
     placeholder="Please input"
   />
+
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { invoke } from '@tauri-apps/api';
 const message = ref('');
+const models = ref('');
+const responses = ref('');
+const sendMessage = async () => {
+    console.log('Message:', message.value);
+    try {
+        const response = await invoke('chat', { message: message.value });
+        responses.value = response as string;
+        console.log('Response:', response);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+const getModels = async () => {
+    try {
+        const response = await invoke('get_models');
+        console.log(response)
+        models.value = response as string;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
 </script>
