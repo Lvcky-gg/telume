8ac90976d5531f59de18a8b7a42de54962ae5bb9
Your Name <matadorcrab@gmail.com>
2025-05-01 14:46:44 -0400

    MY AI IS ALIVE

--- src-tauri/src/conversion/conversions.rs
+++ src-tauri/src/conversion/conversions.rs
@@ -2,10 +2,10 @@ use geoconvert::{LatLon, Mgrs, UtmUps};
 
 #[tauri::command]
 pub fn latlon_to_mgrs(lat: f64, lon: f64) -> String {
-    // let latlon = LatLon::new(lat, lon);
-    // let mgrs = Mgrs::from_latlon(&latlon,10);
-    // mgrs.to_string()
-    return "".to_string();
+    let latlon = LatLon::create(lat, lon).unwrap();
+    let mgrs = Mgrs::from_latlon(&latlon, 6);
+    mgrs.to_string()
+
 }
 #[tauri::command]
 pub fn mgrs_to_latlon(mgrs: &str) -> (f64, f64) {
